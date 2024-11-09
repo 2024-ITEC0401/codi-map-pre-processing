1fc398e0d413a6b6924143fcf669495a7e3124ed
toothlessdev <toothless042@gmail.com>
2024-11-10 01:17:29 +0900

    feature: add function to retrieve google cloud storage file uris

---
+++ codi-map-vertex-ai/src/integration/storage.ts
@@ -0,0 +1,14 @@
+import { Storage } from "@google-cloud/storage";
+
+export async function getBucketMetaData(bucketName: string) {
+  const storage = new Storage();
+  const [metadata] = await storage.bucket(bucketName).getMetadata();
+  console.log(JSON.stringify(metadata, null, 2));
+  return metadata;
+}
+
+export async function listBucketFiles(bucketName: string) {
+  const storage = new Storage();
+  const [files] = await storage.bucket(bucketName).getFiles();
+  return files.map((file) => file.cloudStorageURI.href);
+}
