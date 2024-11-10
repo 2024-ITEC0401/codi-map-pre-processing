0d0aa4cd00df9a116ad7e13fc41ed0acd405de45
toothlessdev <toothless042@gmail.com>
2024-11-10 10:56:11 +0900

    fix: error handling in vertex ai image processing

--- codi-map-vertex-ai/src/analysis.ts
+++ codi-map-vertex-ai/src/analysis.ts
@@ -21,14 +21,17 @@ export async function analyzeImage(cloudStorageUri: string, format = "image/jpg"
       },
     ],
   };
+  try {
+    const response = await model.generateContentStream(request);
+    const result = await response.response;
+    if (!(result.candidates && result.candidates.length > 0)) throw new Error("No Response");
 
-  const response = await model.generateContentStream(request);
-  const result = await response.response;
-  if (!(result.candidates && result.candidates.length > 0)) throw new Error("No Response");
-
-  const data = result.candidates[0].content.parts.reduce((acc, part) => acc + part.text, "");
-  const processedJsonData = data.replaceAll("```", "").replace("json", "").trim();
-  const parsedJsonData = JSON.parse(processedJsonData);
-
-  return parsedJsonData;
+    const data = result.candidates[0].content.parts.reduce((acc, part) => acc + part.text, "");
+    const processedJsonData = data.replaceAll("```", "").replace("json", "").trim();
+    const parsedJsonData = JSON.parse(processedJsonData);
+    return parsedJsonData;
+  } catch (e) {
+    console.log("[VERTEXAI] Error Analyzing Image : ", e);
+    return "";
+  }
 }
